7c1738d6894d0acb42494513f1c608a069175e33
VOLUEAD\konrad.gadecki <konrad.gadecki@volue.com>
2023-05-13 15:00:16 +0200

    created React and TypeScript boilerplate for web application

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import Layout from "./components/Layout";
+
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
